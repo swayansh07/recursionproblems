471924309478d3966d51737fc10dcfa048a9fee0
swayansh07 <swayanshprusty@gmail.com>
2026-02-12 15:02:21 +0530

    Create SumOfNaturalNo.java

---
+++ SumOfNaturalNo.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public static int findSum(int n) 
+    {
+        if (n == 0) 
+        {
+            return 0;
+        }
+        
+        return n + findSum(n - 1);
+    }
+}
