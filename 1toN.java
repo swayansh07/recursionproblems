31891dab8cb690c20fd8bb12fed424ab649c91cb
swayansh07 <swayanshprusty@gmail.com>
2026-02-12 14:28:18 +0530

    Create 1toN.java

---
+++ 1toN.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void printTillN(int n) {
+        
+        if (n == 0)
+        {
+            return;
+        }
+        
+        printTillN(n - 1);
+        
+        System.out.print(n + " ");
+    }
+}
