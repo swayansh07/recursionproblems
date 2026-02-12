ebf676f638e3cfd035851592165bdcb47b582fcf
swayansh07 <swayanshprusty@gmail.com>
2026-02-12 15:11:00 +0530

    Create FactorialofN.java

---
+++ FactorialofN.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public static long factorial(int n) 
+    {
+        
+        if (n == 0 || n == 1) 
+        {
+            return 1;
+        }
+        
+        return n * factorial(n - 1);
+    }
+}
