5d7442465029f1658140c271fdf0708f60b51033
swayansh07 <swayanshprusty@gmail.com>
2026-02-12 14:38:35 +0530

    Create Nto1.java

---
+++ Nto1.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    void printNos(int N) 
+    {
+        if (N == 0)
+        {
+            return;
+        }
+        
+        System.out.print(N + " ");
+        
+        printNos(N - 1);
+    }
+}
